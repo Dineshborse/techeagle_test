962988174e95953a548579ba9aa4b6cde3933d07
Dineshborse <dineshborse3791@gmail.com>
2022-10-17 17:47:07 +0530

    new commit2

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.use(express.json({ limit: "30mb" }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 
 
-app.listen(process.env.PORT || 5000, (err) => {
+app.listen(process.env.PORT || 80, (err) => {
     if (!err) {
-        console.log("server started on port 5000");
+        console.log("server started on port 80");
         console.log(`server running on ${process.env.PORT}`)
     }
     else {
